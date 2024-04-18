9ac0822aebae38d9dad791be1fa78393c7c1ea29
j-pcgoncalves <joao_goncalves_7@hotmail.com>
2024-04-18 22:25:38 +0100

    style a maze row

--- src/MazeGrid.jsx
+++ src/MazeGrid.jsx
@@ -3,10 +3,27 @@ import './App.css'
 export default function MazeGrid() {
   return (
     <div>
-      <div className='cell wall'></div>
-      <div className='cell path'></div>
-      <div className='cell start'></div>
-      <div className='cell end'></div>
+      <div className='row'>
+        <div className='cell wall'></div>
+        <div className='cell path'></div>
+        <div className='cell start'></div>
+        <div className='cell end'></div>
+        <div className='cell wall'></div>
+      </div>
+      <div className='row'>
+        <div className='cell wall'></div>
+        <div className='cell wall'></div>
+        <div className='cell wall'></div>
+        <div className='cell path'></div>
+        <div className='cell wall'></div>
+      </div>
+      <div className='row'>
+        <div className='cell wall'></div>
+        <div className='cell wall'></div>
+        <div className='cell wall'></div>
+        <div className='cell path'></div>
+        <div className='cell wall'></div>
+      </div>
     </div>
   )
 }
