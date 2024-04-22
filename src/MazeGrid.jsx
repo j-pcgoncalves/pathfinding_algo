5a5aa732e807e50af6c088759605c0e9dd2cd127
j-pcgoncalves <joao_goncalves_7@hotmail.com>
2024-04-22 20:14:56 +0100

    generate a maze matrix

--- src/MazeGrid.jsx
+++ src/MazeGrid.jsx
@@ -11,9 +11,33 @@ export default function MazeGrid() {
     ["wall", "wall", "wall", "wall"],
   ];
 
+  // Function that automatically generates a maze
+  function generateMaze(height, width) {
+    let matrix = [];
+
+    // Create a for loop that generates a row depending on the specified height (nº of rows)
+    for (let i = 0; i < height; i++) {
+      let row = [];
+
+      // Create a for loop that generates a cell depending on the specified width (nº of cells)
+      for (let j = 0; j < width; j++) {
+        let cell = Math.random();
+        if (cell < 0.5) {
+          row.push("wall");
+        } else {
+          row.push("path");
+        }
+      }
+
+      matrix.push(row);
+    }
+
+    console.log(matrix);
+  }
+
   return (
     <div className='maze-grid'>
-      <button className='maze-button'>Refresh Maze</button>
+      <button className='maze-button' onClick={() => generateMaze(5, 6)}>Refresh Maze</button>
       <div className='maze'>
         {/* Displaying the maze grid with the map method to through each row and cell of the array */}
         {maze.map((row, rowIndex) => (
