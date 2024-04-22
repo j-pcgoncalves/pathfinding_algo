6a347f09c166560dadb52d0e71f980b9647ed2c0
j-pcgoncalves <joao_goncalves_7@hotmail.com>
2024-04-22 20:07:05 +0100

    create and style a refresh maze button

--- src/MazeGrid.jsx
+++ src/MazeGrid.jsx
@@ -1,6 +1,8 @@
 import './App.css'
 
 export default function MazeGrid() {
+
+  // A 2d array that has grid info
   let maze = [
     ["wall", "wall", "wall", "wall"],
     ["start", "path", "path", "wall"],
@@ -10,14 +12,18 @@ export default function MazeGrid() {
   ];
 
   return (
-    <div>
-      {maze.map((row, rowIndex) => (
-        <div key={rowIndex} className='row'>
-          {row.map((cell, cellIndex) => (
-            <div key={cellIndex} className={`cell ${cell}`}></div>
-          ))}
-        </div>
-      ))}
+    <div className='maze-grid'>
+      <button className='maze-button'>Refresh Maze</button>
+      <div className='maze'>
+        {/* Displaying the maze grid with the map method to through each row and cell of the array */}
+        {maze.map((row, rowIndex) => (
+          <div key={rowIndex} className='row'>
+            {row.map((cell, cellIndex) => (
+              <div key={cellIndex} className={`cell ${cell}`}></div>
+            ))}
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
